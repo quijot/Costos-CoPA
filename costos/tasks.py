3888feddfe66e4ef515b37f1b83490b1b8be9b73
Santiago Pestarini <santiagonob@gmail.com>
2020-12-07 00:48:51 -0300

    Trying to fix DatabaseError SSL error
    Signed-off-by: Santiago Pestarini <santiagonob@gmail.com>

--- costos/tasks.py
+++ costos/tasks.py
@@ -7,6 +7,8 @@ from costos import models
 
 def cotizacion_dolar():
     """Scrap cotización del Dólar desde el sitio del BNA"""
+    from django.db import connection
+    connection.close()
     bna = requests.get("https://bna.com.ar/")
     cotizacion = decimal.Decimal(bna.text.partition("U.S.A")[2].split("<td>")[2].partition("<")[0].replace(",", "."))
     if cotizacion:
