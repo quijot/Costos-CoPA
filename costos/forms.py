53f4ef2cda0978ea7d083723f7a5bed077239d45
Santiago Pestarini <santiagonob@gmail.com>
2021-03-15 11:39:39 -0300

    Better integration with crispy_forms
    Signed-off-by: Santiago Pestarini <santiagonob@gmail.com>

--- costos/forms.py
+++ costos/forms.py
@@ -1,6 +1,6 @@
-from crispy_forms.bootstrap import FormActions, Tab, TabHolder
+from crispy_forms.bootstrap import Alert, AppendedText, FormActions, PrependedText, Tab, TabHolder
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import HTML, Button, Div, Field, Fieldset, Layout, Row, Submit
+from crispy_forms.layout import Button, Div, Field, Fieldset, Layout, Row, Submit
 from django import forms
 from django.contrib.auth.forms import UserChangeForm
 from django.urls import reverse_lazy
@@ -49,23 +49,19 @@ class EmpresaForm(forms.ModelForm):
                         "Características",
                         Row(
                             Div("nombre", css_class="col-lg-8"),
-                            Div("horas_semanales", css_class="col-lg-4"),
+                            Div(AppendedText("horas_semanales", "hs/sem"), css_class="col-lg-4"),
                         ),
                     ),
                     Fieldset(
                         "Gastos",
-                        HTML(
-                            """<div class="alert alert-primary alert-dismissible fade show" role="alert">
-                            Cargar todos los gastos asociados únicamente a la Empresa / Oficina.
+                        Alert(
+                            content="""Cargar todos los gastos asociados únicamente a la Empresa / Oficina.
                             NO se cargan aquí los gastos asociados a los Profesionales que la conforman
                             (salvo que así lo decidan sus miembros).
                             Ejemplos:
                             Alquiler, Impuestos y Servicios propios de la locación, Limpieza, Publicidad,
-                            Hosting, Licencias de Software, Sueldos, etc.
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Cerrar">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                            </div>"""
+                            Hosting, Licencias de Software, Sueldos, etc.""",
+                            css_class="alert-primary alert-dismissible fade show",
                         ),
                         Formset("gastos"),
                         Div(
@@ -140,18 +136,14 @@ class ProfesionalForm(UserChangeForm):
                     ),
                     Fieldset(
                         "Gastos",
-                        HTML(
-                            """<div class="alert alert-info alert-dismissible fade show" role="alert">
-                            Cargar todos los gastos asociados únicamente a este Profesional.
+                        Alert(
+                            content="""Cargar todos los gastos asociados únicamente a este Profesional.
                             NO se cargan aquí los gastos asociados a la Empresa / Oficina.
                             Ejemplos:
                             Matrícula, Monotributo, Jubilación, Obra Social, posiblemente algún Seguro,
                             Capacitación, Celular, etc. <strong>Los gastos asociados a un Profesional
-                            formaran parte del costo por hora de su <em>mano de obra</em></strong>.
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Cerrar">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                            </div>"""
+                            formaran parte del costo por hora de su <em>mano de obra</em></strong>.""",
+                            css_class="alert-info alert-dismissible fade show",
                         ),
                         Formset("gastos"),
                         Div(
@@ -209,32 +201,28 @@ class InstrumentoForm(forms.ModelForm):
                             Div("nombre", css_class="col-lg-12"),
                         ),
                         Row(
-                            Div("valor_USD", css_class="col-lg-6"),
-                            Div("vida_util", css_class="col-lg-6"),
+                            Div(PrependedText("valor_USD", "U$D"), css_class="col-lg-6"),
+                            Div(AppendedText("vida_util", "jornadas"), css_class="col-lg-6"),
                         ),
                         Row(
                             Div(
-                                Div(
-                                    HTML(
-                                        """¿Cómo calcular la <em>Vida útil</em> del instrumento?
-                                        <small id="hint_id_vida_util" class="form-text text-muted">
-                                        Estimar los siguientes valores:<br>
-                                        <strong>A</strong>: cantidad de años en que quisiera recuperar
-                                        el valor invertido y <strong>reponer el instrumento</strong>,<br>
-                                        <strong>T</strong>: promedio de trabajos realizados por año,<br>
-                                        <strong>J</strong>: jornadas de este instrumento que se suelen
-                                        dedicar a cada trabajo. Ejemplo: para una Estación Total puede
-                                        ser 1 jornada por trabajo, pero para una PC pueden ser 4.<br>
-                                        <strong>Resultado</strong>: haciendo <strong>A &times; T &times; J</strong>
-                                        se obtiene un número aproximado de jornadas de vida útil.<br>
-                                        <strong>Ejemplo</strong>: Estación Total, A=5 (5 años de vida útil),
-                                        T=40 (40 trabajos por año), J=1 (1 jornada por trabajo). 5 &times; 40
-                                        &times; 1 = 200 jornadas de vida útil.</small>
-                                        <button type="button" class="close" data-dismiss="alert" aria-label="Cerrar">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>"""
-                                    ),
-                                    css_class="alert alert-warning alert-dismissible fade show",
+                                Alert(
+                                    content="""¿Cómo calcular la <em>Vida útil</em> del instrumento?
+                                    <small id="hint_id_vida_util" class="form-text text-muted">
+                                    Estimar los siguientes valores:<br>
+                                    <strong>A</strong>: cantidad de años en que quisiera recuperar
+                                    el valor invertido y <strong>reponer el instrumento</strong>,<br>
+                                    <strong>T</strong>: promedio de trabajos realizados por año,<br>
+                                    <strong>J</strong>: jornadas de este instrumento que se suelen
+                                    dedicar a cada trabajo. Ejemplo: para una Estación Total puede
+                                    ser 1 jornada por trabajo, pero para una PC pueden ser 4.<br>
+                                    <strong>Resultado</strong>:
+                                    haciendo <strong>A &times; T &times; J</strong> se obtiene un
+                                    número aproximado de jornadas de vida útil.<br>
+                                    <strong>Ejemplo</strong>: Estación Total, A=5 (5 años de vida útil),
+                                    T=40 (40 trabajos por año), J=1 (1 jornada por trabajo). 5 &times;
+                                    40 &times; 1 = 200 jornadas de vida útil.</small>""",
+                                    css_class="alert-warning alert-dismissible fade show",
                                 ),
                                 css_class="col",
                             )
@@ -274,30 +262,30 @@ class VehiculoForm(forms.ModelForm):
                         ),
                         Row(
                             Div("nombre", css_class="col-lg-8"),
-                            Div("valor", css_class="col-lg-4"),
+                            Div(PrependedText("valor", "AR$"), css_class="col-lg-4"),
                         ),
                         Row(
-                            Div("kilometraje_anual", css_class="col-lg-4"),
+                            Div(AppendedText("kilometraje_anual", "km/año"), css_class="col-lg-4"),
                             Div("tipo_combustible", css_class="col-lg-4"),
-                            Div("rendimiento", css_class="col-lg-4"),
+                            Div(AppendedText("rendimiento", "km/lt"), css_class="col-lg-4"),
                         ),
                     ),
                     Fieldset(
                         "Mantenimiento",
                         Row(
-                            Div("costo_patente", css_class="col-lg-4"),
-                            Div("costo_seguro", css_class="col-lg-4"),
-                            Div("costo_cochera", css_class="col-lg-4"),
+                            Div(PrependedText("costo_patente", "$"), css_class="col-lg-4"),
+                            Div(PrependedText("costo_seguro", "$"), css_class="col-lg-4"),
+                            Div(PrependedText("costo_cochera", "$"), css_class="col-lg-4"),
                         ),
                         Row(
-                            Div("costo_lubricante", css_class="col-lg-4"),
-                            Div("costo_lavado", css_class="col-lg-4"),
-                            Div("costo_neumatico", css_class="col-lg-4"),
+                            Div(PrependedText("costo_lubricante", "$"), css_class="col-lg-4"),
+                            Div(PrependedText("costo_lavado", "$"), css_class="col-lg-4"),
+                            Div(PrependedText("costo_neumatico", "$"), css_class="col-lg-4"),
                         ),
                         Row(
-                            Div("costo_service", css_class="col-lg-4"),
-                            Div("costo_anual_reparaciones", css_class="col-lg-4"),
-                            Div("costo_rto", css_class="col-lg-4"),
+                            Div(PrependedText("costo_service", "$"), css_class="col-lg-4"),
+                            Div(PrependedText("costo_anual_reparaciones", "$"), css_class="col-lg-4"),
+                            Div(PrependedText("costo_rto", "$"), css_class="col-lg-4"),
                         ),
                     ),
                     FormActions(
@@ -418,14 +406,10 @@ class TrabajoForm(forms.ModelForm):
                 self.initial[pref] = user.preferences[f"trabajo__{pref}"]
         self.helper = FormHelper()
         self.helper.layout = Layout(
-            HTML(
-                """<div class="alert alert-danger alert-dismissible fade show" role="alert">
-            Antes de guardar, recuerde asignar al menos un profesional actuante en la
-            pestaña <strong>Profesionales.</strong>
-            <button type="button" class="close" data-dismiss="alert" aria-label="Cerrar">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            </div>"""
+            Alert(
+                content="""Antes de guardar, recuerde asignar al menos un profesional
+                actuante en la pestaña <strong>Profesionales.</strong>""",
+                css_class="alert-danger alert-dismissible fade show",
             ),
             TabHolder(
                 Tab(
@@ -434,47 +418,47 @@ class TrabajoForm(forms.ModelForm):
                         "",
                         Row(
                             Div(Field("fecha", css_class="date", id="datepicker"), css_class="col-lg-3"),
-                            Div("expediente", css_class="col-lg-3"),
+                            Div(PrependedText("expediente", "N&ordm;"), css_class="col-lg-3"),
                             Div("comitente", css_class="col-lg-6"),
                         ),
                     ),
                     Fieldset(
                         "Gastos típicos de Mensura",
                         Row(
-                            Div("aporte_copa", css_class="col-lg-6"),
-                            Div("aporte_caja", css_class="col-lg-6"),
+                            Div(PrependedText("aporte_copa", "$"), css_class="col-lg-6"),
+                            Div(PrependedText("aporte_caja", "$"), css_class="col-lg-6"),
                         ),
                         Row(
                             Div("partidas", css_class="col-lg-3"),
                             Div("lotes_finales", css_class="col-lg-3"),
-                            Div("escrituras", css_class="col-lg-3"),
-                            Div("visados", css_class="col-lg-3"),
+                            Div(PrependedText("escrituras", "$"), css_class="col-lg-3"),
+                            Div(PrependedText("visados", "$"), css_class="col-lg-3"),
                         ),
                     ),
                     Fieldset(
                         "Gastos en jornadas de medición",
                         Row(
-                            Div("ayudante", css_class="col-lg-4"),
-                            Div("viaticos", css_class="col-lg-4"),
-                            Div("mojones", css_class="col-lg-4"),
+                            Div(PrependedText("ayudante", "$"), css_class="col-lg-4"),
+                            Div(PrependedText("viaticos", "$"), css_class="col-lg-4"),
+                            Div(PrependedText("mojones", "$"), css_class="col-lg-4"),
                         ),
                         Row(
-                            Div("alquiler_instrumentos", css_class="col-lg-6"),
-                            Div("seguros_especiales", css_class="col-lg-6"),
+                            Div(PrependedText("alquiler_instrumentos", "$"), css_class="col-lg-6"),
+                            Div(PrependedText("seguros_especiales", "$"), css_class="col-lg-6"),
                         ),
                     ),
                     Fieldset(
                         "Gastos por presentaciones",
                         Row(
-                            Div("dibujante", css_class="col-lg-4"),
-                            Div("impresiones", css_class="col-lg-4"),
-                            Div("gestor", css_class="col-lg-4"),
+                            Div(PrependedText("dibujante", "$"), css_class="col-lg-4"),
+                            Div(PrependedText("impresiones", "$"), css_class="col-lg-4"),
+                            Div(PrependedText("gestor", "$"), css_class="col-lg-4"),
                         ),
                     ),
                     Fieldset(
                         "Otros",
                         Row(
-                            Div("otros_gastos", css_class="col-lg-12"),
+                            Div(PrependedText("otros_gastos", "$"), css_class="col-lg-12"),
                         ),
                     ),
                 ),
@@ -525,10 +509,10 @@ class TrabajoForm(forms.ModelForm):
                     Fieldset(
                         "Gastos por trámites extra",
                         Row(
-                            Div("ccu", css_class="col-lg-3"),
-                            Div("estudio_titulos", css_class="col-lg-3"),
-                            Div("georreferenciacion", css_class="col-lg-3"),
-                            Div("citaciones", css_class="col-lg-3"),
+                            Div(PrependedText("ccu", "$"), css_class="col-lg-3"),
+                            Div(PrependedText("estudio_titulos", "$"), css_class="col-lg-3"),
+                            Div(PrependedText("georreferenciacion", "$"), css_class="col-lg-3"),
+                            Div(PrependedText("citaciones", "$"), css_class="col-lg-3"),
                         ),
                     ),
                 ),
@@ -553,45 +537,41 @@ def user_preferences_form(instance, section=None):
         TabHolder(
             Tab(
                 "Trabajos",
-                HTML(
-                    """<div class="alert alert-dark alert-dismissible fade show" role="alert">
-                Cargue aquí los valores más habituales para sus trabajos, de esta manera,
-                se cargarán <strong>por defecto</strong> cada vez que agregue un nuevo Trabajo.
-                <button type="button" class="close" data-dismiss="alert" aria-label="Cerrar">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                </div>"""
+                Alert(
+                    content="""Cargue aquí los valores más habituales para sus trabajos, de esta manera,
+                    se cargarán <strong>por defecto</strong> cada vez que agregue un nuevo Trabajo.""",
+                    css_class="alert-dark alert-dismissible fade show",
                 ),
                 Fieldset(
                     "Gastos típicos de Mensura",
                     Row(
-                        Div("trabajo__visados", css_class="col-lg-3"),
+                        Div(PrependedText("trabajo__visados", "$"), css_class="col-lg-3"),
                     ),
                 ),
                 Fieldset(
                     "Gastos en jornadas de medición",
                     Row(
-                        Div("trabajo__ayudante", css_class="col-lg-4"),
-                        Div("trabajo__viaticos", css_class="col-lg-4"),
-                        Div("trabajo__mojones", css_class="col-lg-4"),
+                        Div(PrependedText("trabajo__ayudante", "$"), css_class="col-lg-4"),
+                        Div(PrependedText("trabajo__viaticos", "$"), css_class="col-lg-4"),
+                        Div(PrependedText("trabajo__mojones", "$"), css_class="col-lg-4"),
                     ),
                     Row(
-                        Div("trabajo__alquiler_instrumentos", css_class="col-lg-6"),
-                        Div("trabajo__seguros_especiales", css_class="col-lg-6"),
+                        Div(PrependedText("trabajo__alquiler_instrumentos", "$"), css_class="col-lg-6"),
+                        Div(PrependedText("trabajo__seguros_especiales", "$"), css_class="col-lg-6"),
                     ),
                 ),
                 Fieldset(
                     "Gastos por presentaciones",
                     Row(
-                        Div("trabajo__dibujante", css_class="col-lg-4"),
-                        Div("trabajo__impresiones", css_class="col-lg-4"),
-                        Div("trabajo__gestor", css_class="col-lg-4"),
+                        Div(PrependedText("trabajo__dibujante", "$"), css_class="col-lg-4"),
+                        Div(PrependedText("trabajo__impresiones", "$"), css_class="col-lg-4"),
+                        Div(PrependedText("trabajo__gestor", "$"), css_class="col-lg-4"),
                     ),
                 ),
                 Fieldset(
                     "Otros",
                     Row(
-                        Div("trabajo__otros_gastos", css_class="col-lg-12"),
+                        Div(PrependedText("trabajo__otros_gastos", "$"), css_class="col-lg-12"),
                     ),
                 ),
             ),
